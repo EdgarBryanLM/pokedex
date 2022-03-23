5593a681087b97eefde7e4abf87c25a5b3b2e294
EdgarBryanLM <s17120227@alumnos.itsur.edu.mx>
2022-03-23 14:12:29 -0600

    cambios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../services/pokemon.service';
+import { pokemon } from '../interfaces/pokemon.interface';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,14 @@ import { PokemonService } from '../services/pokemon.service';
 export class HomeComponent implements OnInit {
 
   constructor(private servicio:PokemonService) { }
-
+    Pokemon!:pokemon;
   ngOnInit() {
 
-    this.servicio.getPokemon().subscribe(res => {
+    this.servicio.getPokemon().subscribe((res:pokemon) => {
 
-      console.log(res);
+      this.Pokemon=res;
+      console.log(this.Pokemon.sprites.other?.home.front_default);
+      
 
     });
   }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pokemon } from '../interfaces/pokemon.interface';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +10,6 @@ export class PokemonService {
   url:string='https://pokeapi.co/api/v2/pokemon/ditto';
 
   getPokemon(){
-    return this.http.get(this.url);
+    return this.http.get<pokemon>(this.url);
   }
 }
